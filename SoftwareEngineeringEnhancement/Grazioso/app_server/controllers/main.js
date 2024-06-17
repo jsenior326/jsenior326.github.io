f8860167af797c5a6925e538ac36024b4c5bcdbe
jsenior326 <57270866+jsenior326@users.noreply.github.com>
2024-06-17 00:35:40 -0500

    Maintenance of Software engineering enhancement
    Unable to upload app_admin folder

--- SoftwareEngineeringEnhancement/Grazioso/app_server/controllers/main.js
+++
@@ -1,8 +0,0 @@
-// Get homepage
-const index = (req, res) => {
-    res.render('index', { title: "Grazioso Salvare"});
-};
-
-module.exports = {
-    index
-}
--- SoftwareEngineeringEnhancement/Grazioso/app_server/routes/animals.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/animals.js');
-
-// Get animals
-router.get('/', controller.animals);
-
-module.exports = router;
